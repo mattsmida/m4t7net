6172718af6a0faee6e2af55820e7bf97b690c6d4
Matt Smida <mcs@m4t7.com>
2023-10-25 16:30:58 -0600

    general cleanup

--- htdocs/rithm/meme-script.js
+++ htdocs/rithm/meme-script.js
@@ -1,24 +1,12 @@
-// Write something to console.log the contents of the form on click.
-
-
-
-// Add an event listener to the button
-// Save the form inputs to variables
-// Console log the variable values
-// Clear the form
-
 let memeForm = document.getElementById('meme-form');
 let memeGoButton = document.getElementById('meme-go-button');
 let delButtons = document.querySelectorAll('button.del');
 assignDelButtons();
 memeForm.addEventListener('submit', function(e) {
-  //event.preventDefault();  // Original when function() had no param
   e.preventDefault();
   let url = document.getElementById('meme-url');
   let topText = document.getElementById('meme-top-text');
   let bottomText = document.getElementById('meme-bottom-text');
-
-  // Make a new meme div with divs pic, top-text, bottom-text.
   let htmlBefore = document.getElementById('meme-zone').innerHTML;
 
   document.getElementById('meme-zone').innerHTML = '\
@@ -29,7 +17,6 @@ memeForm.addEventListener('submit', function(e) {
     <div class="bottom-text">' + bottomText.value.toUpperCase() + '</div>\
     <br></div>' + htmlBefore;
 
-  // Reset the form
   this.reset();
   assignDelButtons();
 });
