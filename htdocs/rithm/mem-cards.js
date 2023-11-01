aecfae1ac738f5c2aa88ccb85ba5f31e07738435
Matt Smida <mcs@m4t7.com>
2023-10-31 19:14:21 -0600

    js to populate card grid as well as some flip logic

--- htdocs/rithm/mem-cards.js
+++ htdocs/rithm/mem-cards.js
@@ -4,14 +4,13 @@
 
 const FOUND_MATCH_WAIT_MSECS = 1000;
 const COLORS = [
-  "red", "blue", "green", "orange", "purple",
-  "red", "blue", "green", "orange", "purple",
+  "red", "blu", "gre", "ora", "pur",
+  "red", "blu", "gre", "ora", "pur",
 ];
 
 const colors = shuffle(COLORS);
 
 createCards(colors);
-
 
 /** Shuffle array items in-place and return shuffled array. */
 
@@ -33,33 +32,47 @@ function shuffle(items) {
 
 /** Create card for every color in colors (each will appear twice)
  *
- * Each div DOM element will have:
+ * Each ~~div~~ td DOM element will have:
  * - a class with the value of the color
  * - a click event listener for each card to handleCardClick
  */
 
 function createCards(colors) {
-  const gameBoard = document.getElementById("game");
+  const gameBoard = document.getElementById("game"); // Given by RS.
+  let cardNum = 0;
+  let newTd;
+  const newTr = document.createElement('tr');
 
-  for (let color of colors) {
-    // missing code here ...
+  for (let color of colors) {   // Given by RS.
+    // Fill in the table child of #game
+    // First version will have two rows with five cells each, so </tr> after 5.
+    // Each td gets id of 'card-xx' and classes 'card ccc' where ccc is color.
+    if (cardNum % 5 === 0) { gameBoard.querySelector('table').append(newTr); }
+    newTd = document.createElement('td');
+    newTd.id = 'card-0' + cardNum.toString(); // TODO: padding sans hardcode.
+    newTd.className = 'card ' + color;
+    newTd.addEventListener('click', function(e) {
+      flipCard(e.target); // TODO: instead of fC(), use hCC() to flip or not.
+    });
+    gameBoard.querySelector('table').append(newTd);
+    cardNum++;
   }
 }
 
 /** Flip a card face-up. */
-
 function flipCard(card) {
-  // ... you need to write this ...
+  card.classList.add('up');
+  setTimeout(() => {
+  card.classList.remove('up');
+  }, 1000);
 }
 
 /** Flip a card face-down. */
-
 function unFlipCard(card) {
-  // ... you need to write this ...
+  // TODO: Write this.
 }
 
 /** Handle clicking on a card: this could be first-card or second-card. */
-
 function handleCardClick(evt) {
-  // ... you need to write this ...
+  // TODO: Write this.
 }
